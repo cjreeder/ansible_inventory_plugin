abe041d325eb1432b20cdfd4a0a9698be501d2d3
Clinton Reeder <clinton_reeder@byu.edu>
2021-01-28 13:18:24 -0700

    fixing plugin to be compatible with ansible

--- inventory_plugins/couch_inventory.py
+++ inventory_plugins/couch_inventory.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from _future_ import (absolute_import, division, print_function)
+from __future__ import (absolute_import, division, print_function)
 _metaclass_ = type
 
 DOCUMENTATION = r'''
@@ -20,11 +20,27 @@ DOCUMENTATION = r'''
       description: Web location of the rooms for the inventory file
       required: true
 '''
-import ansible.module_utils.six.moves.urllib.request as urllib_request
-import ansible.module_utils.six.moves.urllib.error as urllib_error
+from urllib.request import urlopen
+import urllib.error
+import json
+import socket
+import re
 
 from ansible.plugins.inventory import BaseInventoryPlugin
 from ansible.errors import AnsibleError, AnsibleParserError
+
+from collections import defaultdict
+
+lists = []
+CP = []
+SP = []
+STB = []
+MSD = []
+DS = []
+production = []
+stage = []
+no_designation = []
+buildings = []
 
 class InventoryModule(BaseInventoryPlugin):
     Name = 'couch_inventory'
@@ -32,73 +48,79 @@ class InventoryModule(BaseInventoryPlugin):
     def verify_file(self, path):
         '''Return true/false if this is possibly a valid file for this plugin to consume'''
         super(InventoryModule, self).verify_file(path)
-        return path.endswith(('couch_inventory.yaml', 'couch_inventory.yml'))
+        valid = False
+        if super(InventoryModule, self).verify_file(path):
+            if path.endswith(('couch_inventory.yaml', 'couch_inventory.yml')):
+                valid = True
+        return valid
 
-    def device_Designation(l, rooms_location):
+    def device_Designation(self, l, rooms_location):
         hostname = l  
-        print(hostname)
         host_name_split=hostname.split("-")
         building_name = host_name_split[0]
         room_number = host_name_split[1]
         room = host_name_split[0] + '-' + host_name_split[1]
-        if buildings[building_name]:
-            buildings[building_name].append(hostname)
-        else:
-            buildings[building_name] = [hostname]
-
+        if building_name not in buildings:
+            self.inventory.add_group(building_name)
+            buildings.append(building_name)
+        self.inventory.add_host(host=hostname, group=building_name)
+        
         try:
             room_response = urlopen(rooms_location+room)
             rr=room_response.read()
             rr_json = json.loads(rr)
             desig = rr_json['designation']
         except urllib.error.HTTPError:
-            print("Cannot Get designation")
             desig = 'no_designation'
-
+        
         if desig=='production':
-            print(desig)
-            production.append(hostname)
+            #production.append(hostname)
+            self.inventory.add_host(host=hostname, group=desig)
         elif desig=='stage':
-            stage.append(hostname)
-            print(desig)
+            #stage.append(hostname)
+            self.inventory.add_host(host=hostname, group=desig)
         else:
-            no_designation.append(hostname)
-            print(desig)
+            #no_designation.append(hostname)
+            self.inventory.add_host(host=hostname, group="no_designation")
     
-    def couch_inventory(self, devices, rooms):
+    def couch_inventory(self, devices_url, rooms_url):
+        self.inventory.add_group('CP')
+        self.inventory.add_group('SP')
+        self.inventory.add_group('STB')
+        self.inventory.add_group('MSD')
+        self.inventory.add_group('DS')
+        self.inventory.add_group('production')
+        self.inventory.add_group('stage')
+        self.inventory.add_group('no_designation')
+
         try: 
-            response = urlopen(devices)
+            response = urlopen(self.devices_url)
             resp = response.read()
             resp_json = json.loads(resp)
         except urllib.error.HTTPError:
             print("Cannot get information from database")
             return -1
+        
         for row in resp_json["rows"]:
             lists.append(row['id'])
-
-
+        
         for l in lists:
             if re.search(r'-CP[0-9]+\b', l):
-                CP.append(l)
-                device_Designation(l)
+                self.device_Designation(l, rooms_url)
+                self.inventory.add_host(host=l, group='CP')
             elif re.search(r'-SP[0-9]+\b', l):
-                SP.append(l)
-                device_Designation(l)
+                self.device_Designation(l, rooms_url)
+                self.inventory.add_host(host=l, group='SP')
             elif re.search(r'-STB[0-9]+\b', l):
-                STB.append(l)
-                device_Designation(l)
+                self.device_Designation(l, rooms_url)
+                self.inventory.add_host(host=l, group='STB')
             elif re.search(r'-MSD[0-9]+\b', l):
-                MSD.append(l)
-                device_Designation(l)
+                self.device_Designation(l, rooms_url)
+                self.inventory.add_host(host=l, group='MSD')
             elif re.search(r'-DS[0-9]+\b', l):
-                DS.append(l)
-                device_Designation(l)
-
-        d = {"all": [{"Control Processors":CP},{"Scheduling Panels":SP},{"Set-top Boxes":STB},{"Portable Set-top Boxes":MSD},{"Divider Sensors":DS},{"Production":production},{"Stage":stage},{"No Designation":no_designation},{"Buildings":buildings}]}
-        return d 
-        #with open('data.txt', 'w') as outfile:
-        #    json.dump(d, outfile)
-
+                self.device_Designation(l, rooms_url)
+                self.inventory.add_host(host=l, group='DS')
+       
     def parse(self, inventory, loader, path, cache):
         '''Return dynamic inventory from source'''
         super(InventoryModule, self).parse(inventory, loader, path)
@@ -106,17 +128,15 @@ class InventoryModule(BaseInventoryPlugin):
         # Read the inventory YAML file
         self._read_config_data(path)
     
-    try:
-        self.plugin = self.get_option('plugin')
-        self.dev_url = self.get_option('devices_url')
-        self.rm_url = self.get_option('rooms_url')
-    except Exception as e:
-        raise AnsibleParserError('All correction options required: {}'.format(e))
+        try:
+            self.plugin = self.get_option('plugin')
+            self.devices_url = self.get_option('devices_url')
+            self.rooms_url = self.get_option('rooms_url')
+        except Exception as e:
+            raise AnsibleParserError('All correction options required: {}'.format(e))
 
-    self.couch_populate()
+        self.couch_inventory(self.devices_url, self.rooms_url)
 
-
-    #root_group_name = self.inventoiry.add_group('root_group')
 
     
 
